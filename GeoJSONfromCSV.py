0a0e36a9e010a697bae0e4c33b680c74855140e6
Scott Clement <scottclement14303@gmail.com>
2020-04-09 08:53:31 -0400

    Create GeoJSONfromCSV.py
    Outline of code required to read in data from apis, filter and clean the data, add calculated fields, and write out as geoJSON file.

---
+++ GeoJSONfromCSV.py
@@ -0,0 +1,44 @@
+'''
+geoJSONfromCSV
+Created on Apr 9, 2020
+authors: Scott Clement, Luis Resh
+GES771: Advanced Spatial Data Management
+'''
+
+# imports
+import geopandas as gp
+import json
+
+##### STATES ###############################
+
+# read geoJSON data from states polygons api into geopandas table
+
+
+# read in CSV data from covidtracking api; join with state polygons in table
+
+
+# filter out unneeded data; handle nulls and other data cleanup
+
+
+# add columns and populate with calculated values for infection rates, hospitalization rates, and death rates
+
+
+# export or write out geoJSON object from geopandas table
+
+
+##### COUNTIES ############################
+
+# read geoJSON data from counties polygons api into geopandas table
+
+
+# read in CSV data from ???? api; join with county polygons in table
+
+
+# filter out unneeded data; handle nulls and other data cleanup
+
+
+# add columns and populate with calculated values for infection rates, hospitalization rates, and death rates
+
+
+# export or write out geoJSON object from geopandas table
+
