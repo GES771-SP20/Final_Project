dc58923aa28f4f28870d7aa16bab5a89f1ecb0dd
Scott Clement <scottclement14303@gmail.com>
2020-05-02 15:37:00 -0400

    Add SELECT statement for covid19_maxvalues view

--- src/SelectStatementsForTableViews.sql
+++ src/SelectStatementsForTableViews.sql
@@ -7,3 +7,18 @@ ON covid.state = states.stusps
 WHERE covid.ymd_date IN ('2020-04-03', '2020-04-10', '2020-04-17', '2020-04-24', '2020-05-01')
 ORDER BY covid.state, covid.ymd_date DESC
 ;
+
+/*
+-- covid19_maxvalues
+SELECT 
+	max(positive) as max_pos,
+	max(negative) as max_neg,
+	max(total_tests) as max_total,
+	max(hospitalized) as max_hosp,
+	max(deaths) as max_deaths,
+	max(infect_rate) as max_infect_rate,
+	max(hosp_rate) as max_hosp_rate,
+	max(death_rate) as max_death_rate
+FROM covid19_stats AS covid
+;
+*/
