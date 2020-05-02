9a9c7c8dea9c470ee24c640061e6f9c87ec006d7
Scott Clement <scottclement14303@gmail.com>
2020-05-02 15:36:05 -0400

    Change covid19_states_view to weekly stats

--- src/SelectStatementsForTableViews.sql
+++ src/SelectStatementsForTableViews.sql
@@ -1,7 +1,9 @@
+-- covid19_states_view
 SELECT covid.*, states.name AS state_name, states.geom
 FROM tl_2019_us_state AS states
 LEFT JOIN (SELECT * FROM covid19_stats) AS covid
 ON covid.state = states.stusps
-WHERE covid.ymd_date BETWEEN '20200411' AND '20200414'
-ORDER BY covid.state
+--WHERE covid.ymd_date BETWEEN '20200429' AND '20200501'
+WHERE covid.ymd_date IN ('2020-04-03', '2020-04-10', '2020-04-17', '2020-04-24', '2020-05-01')
+ORDER BY covid.state, covid.ymd_date DESC
 ;
